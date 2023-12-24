127e97dc258bb6316c4a8a0a295149b8a0b40a92
erdnaxeler <55086786+erdnaxeler@users.noreply.github.com>
2023-12-24 00:09:23 -0500

    Updated basic wireframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
 import streamlit as st
 
-header - st.beta_container()
-quizBody - st.beta_container()
-
-with header:
-    st.title('Crée ton profil gustatif')
+st.title('Crée ton profil gustatif')
