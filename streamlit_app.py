03fda631af86c3ac413511f199e5adf4ef3fb150
erdnaxeler <55086786+erdnaxeler@users.noreply.github.com>
2023-12-24 20:15:41 -0500

    Fixing next issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,39 @@
 # main_app.py
 
 import streamlit as st
-from wine_data import wine_data  # Importing the wine data
-
-def rate_wine():
-    # Access the current characteristic
-    characteristic = wine_data[st.session_state.current_characteristic]
-    st.write(f"Rate the {characteristic['characteristic']}:")
-    rating = st.slider("", characteristic["scale"][0], characteristic["scale"][1], 50)
-
-    if st.button("Next"):
-        # Store the rating and proceed to the next characteristic
-        st.session_state.ratings[characteristic['characteristic']] = rating
-        st.session_state.current_characteristic += 1
+from wine_data import wine_data  # Make sure to have the wine_data.py as previously defined
 
 def main():
-    if 'current_characteristic' not in st.session_state:
-        st.session_state.current_characteristic = 0
+    # Initialize session state variables
+    if 'current_characteristic_index' not in st.session_state:
+        st.session_state.current_characteristic_index = 0
     if 'ratings' not in st.session_state:
         st.session_state.ratings = {}
+    if 'button_clicked' not in st.session_state:
+        st.session_state.button_clicked = False
 
     st.title("Wine Rating App")
 
-    if st.session_state.current_characteristic < len(wine_data):
-        rate_wine()
-    else:
+    # Only show rating sliders and button if there are characteristics left to rate
+    if st.session_state.current_characteristic_index < len(wine_data):
+        characteristic = wine_data[st.session_state.current_characteristic_index]
+        st.write(f"Rate the {characteristic['characteristic']}:")
+        rating = st.slider("", characteristic["scale"][0], characteristic["scale"][1], 50, key=characteristic['characteristic'])
+
+        # Button to go to the next characteristic
+        if st.button('Next', key=f'next_{characteristic["characteristic"]}'):
+            st.session_state.ratings[characteristic['characteristic']] = rating
+            st.session_state.current_characteristic_index += 1
+            st.session_state.button_clicked = not st.session_state.button_clicked
+
+    # Check for completion of the rating process
+    elif len(st.session_state.ratings) == len(wine_data):
         st.write("Wine rating completed!")
         st.write("Your ratings:", st.session_state.ratings)
         if st.button("Restart Rating"):
-            st.session_state.current_characteristic = 0
+            st.session_state.current_characteristic_index = 0
             st.session_state.ratings = {}
+            st.session_state.button_clicked = False
 
 if __name__ == "__main__":
     main()
