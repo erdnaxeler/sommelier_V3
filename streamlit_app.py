2d9e20233454a556df691a719a2c8f9dca9bed5e
erdnaxeler <55086786+erdnaxeler@users.noreply.github.com>
2023-12-24 20:13:39 -0500

    Fixing issues with new structure
    external wine data, using insights from previous community questions for a new approach to the quiz

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,35 @@
+# main_app.py
+
 import streamlit as st
+from wine_data import wine_data  # Importing the wine data
 
-# Define the bottles data
-bottles = [
-    {"name": "Blanc de Noirs, Domaine Armand Heitz", "acid": 50, "tanin": 20, "douceur": 0, "corps": 20, "alcool": 40},
-    {"name": "Bourgogne Blanc, Domaine Armand Heitz", "acid": 50, "tanin": 0, "douceur": 0, "corps": 20, "alcool": 40},
-    {"name": "1er Cru Passetemps, Domaine de Villaine", "acid": 100, "tanin": 70, "douceur": 0, "corps": 90, "alcool": 40},
-    {"name": "Pernand-Vergelesses En Caradeux, Maison Champy", "acid": 100, "tanin": 0, "douceur": 0, "corps": 50, "alcool": 40},
-    {"name": "1er Cru Ile des Vergelesses, Domaine Pavelot", "acid": 100, "tanin": 50, "douceur": 0, "corps": 50, "alcool": 40},
-    {"name": "Les Grèves, Domaine de la Vougeraie", "acid": 100, "tanin": 50, "douceur": 0, "corps": 50, "alcool": 40},
-    {"name": "Château Montrose", "acid": 100, "tanin": 100, "douceur": 0, "corps": 90, "alcool": 40},
-    {"name": "Château Mouton Rothshild", "acid": 10, "tanin": 100, "douceur": 0, "corps": 90, "alcool": 80}
-]
+def rate_wine():
+    # Access the current characteristic
+    characteristic = wine_data[st.session_state.current_characteristic]
+    st.write(f"Rate the {characteristic['characteristic']}:")
+    rating = st.slider("", characteristic["scale"][0], characteristic["scale"][1], 50)
 
-def calculate_variance(user_preferences, bottle):
-    variance = sum(abs(user_preferences[criteria] - bottle[criteria]) for criteria in user_preferences)
-    return variance
+    if st.button("Next"):
+        # Store the rating and proceed to the next characteristic
+        st.session_state.ratings[characteristic['characteristic']] = rating
+        st.session_state.current_characteristic += 1
 
-def find_best_match(bottles, user_preferences):
-    variance_scores = [(bottle["name"], calculate_variance(user_preferences, bottle)) for bottle in bottles]
-    sorted_bottles = sorted(variance_scores, key=lambda x: x[1])
-    return sorted_bottles
+def main():
+    if 'current_characteristic' not in st.session_state:
+        st.session_state.current_characteristic = 0
+    if 'ratings' not in st.session_state:
+        st.session_state.ratings = {}
 
-# Streamlit app
-st.title("Wine Preference Quiz")
+    st.title("Wine Rating App")
 
-# Initialize session state
-if 'preferences' not in st.session_state:
-    st.session_state['preferences'] = {}
-if 'current_question_index' not in st.session_state:
-    st.session_state['current_question_index'] = 0
-if 'update_question' not in st.session_state:
-    st.session_state['update_question'] = False
+    if st.session_state.current_characteristic < len(wine_data):
+        rate_wine()
+    else:
+        st.write("Wine rating completed!")
+        st.write("Your ratings:", st.session_state.ratings)
+        if st.button("Restart Rating"):
+            st.session_state.current_characteristic = 0
+            st.session_state.ratings = {}
 
-# Define the questions
-questions = [
-    {"label": "Acidity", "key": "acid"},
-    {"label": "Tanin", "key": "tanin"},
-    {"label": "Douceur", "key": "douceur"},
-    {"label": "Corps", "key": "corps"},
-    {"label": "Alcool", "key": "alcool"}
-]
-
-# Function to update the current question index
-def update_current_question_index():
-    st.session_state['current_question_index'] += 1
-    st.session_state['update_question'] = False
-
-# Display the current question
-if st.session_state['current_question_index'] < len(questions):
-    question = questions[st.session_state['current_question_index']]
-    preference = st.number_input(question['label'], 0, 100, 50, step=1, key=question['key'])
-
-    if st.button('Next', key=f'next_{question["key"]}'):
-        st.session_state['preferences'][question['key']] = preference
-        st.session_state['update_question'] = True
-
-# Update the question index after the button press
-if st.session_state['update_question']:
-    update_current_question_index()
-
-# Show results after the last question
-if st.session_state['current_question_index'] >= len(questions):
-    user_preferences = st.session_state['preferences']
-    best_match = find_best_match(bottles, user_preferences)
-    st.write("Top Matching Bottles:")
-    for bottle in best_match[:3]:
-        st.write(f"Bottle: {bottle[0]}, Variance Score: {bottle[1]}")
-    
-    # Reset Quiz
-    if st.button("Restart Quiz"):
-        st.session_state['current_question_index'] = 0
-        st.session_state['preferences'] = {}
-        st.session_state['update_question'] = False
+if __name__ == "__main__":
+    main()
