d2b690e4e3dba65e7bfd6b159d7841298a4e8968
erdnaxeler <55086786+erdnaxeler@users.noreply.github.com>
2023-12-24 20:12:44 -0500

    Create wine_data.py

---
+++ wine_data.py
@@ -0,0 +1,25 @@
+# wine_data.py
+
+wine_data = [
+    {
+        "characteristic": "Acidity",
+        "scale": [0, 100]  # Min and max values for the scale
+    },
+    {
+        "characteristic": "Tanin",
+        "scale": [0, 100]
+    },
+    {
+        "characteristic": "Douceur",
+        "scale": [0, 100]
+    },
+    {
+        "characteristic": "Corps",
+        "scale": [0, 100]
+    },
+    {
+        "characteristic": "Alcool",
+        "scale": [0, 100]
+    }
+    # You can add more characteristics as needed
+]
